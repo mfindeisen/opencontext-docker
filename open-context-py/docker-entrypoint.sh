b8b1b9255c79f0b38531cc8621916f97b7e9f8c4
mfindeisen <findeisen.matthias@gmail.com>
2019-09-02 22:11:14 +0200

    uwsgi config (WIP)

--- open-context-py/docker-entrypoint.sh
+++ open-context-py/docker-entrypoint.sh
@@ -7,8 +7,13 @@ python3 manage.py createcachetable
 echo "Apply database migrations"
 python3 manage.py makemigrations versioning profiles fieldgroups inputfields inputrelations rules trinomials uri entity redirects namespaces subjects ocitem manifest assertions events geospace mediafiles documents persons projects strings octypes octypetree predicates predicatetree identifiers obsmetadata editorials ocmysql fields fieldannotations records sources linkannotations linkentities expfields exprecords exptables indexer search staticfiles debug_toolbar django_user_agents 
 python3 manage.py migrate
+#python3 manage.py migrate --run-syncdb
 
 # Collect statics
 echo "collect statics"
 python3 manage.py collectstatic --no-input
-exec "$@"
+
+uwsgi --emperor /etc/uwsgi/vassals --uid www-data --gid www-data --http-socket :8000 --ini /src/ocweb/uwsgi_oc.ini
+
+tail -f /dev/null
+#exec "$@"
