b581f82885b1ca5453552b84f93009e53a8a0ecd
mfindeisen <findeisen.matthias@gmail.com>
2021-01-23 15:19:27 +0100

    Add start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # Since we want to mount the volume to develop open-context from the host, we need to clone the oc repository before docker build.
-git clone https://github.com/ekansa/open-context-py.git
+git clone https://github.com/mfindeisen/open-context-py.git --branch docker
 mv ./open-context-py/ ./open_context/
